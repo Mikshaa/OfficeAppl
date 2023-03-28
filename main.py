b1fa0b5953f975b0e726964db7d4448ec8dd4824
Michael <abdulmanov.misha@mail.ru>
2023-03-28 21:44:46 +0300

    load files

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import xlwings, xlrd
+import pandas
+import datetime
+import threading
+import urllib.request
+from time import sleep
+
+
+
+def connect(mode = 'check'):
+    if mode == 'check':
+        host = 'http://google.com'
+    elif mode == 'recheck':
+        host = 'https://ya.ru/'
+    else:
+        return 'ModeError: parametr mode can takes "check" or "recheck"'
+    try:
+        urllib.request.urlopen(host)
+        return True
+        connected = True
+    except:
+        return False
+        connected = False
+    if not connected:
+        print('окно')
+
+
+
+if connect():
+    print('connected')
+elif connect(mode='recheck'):
+    print('connected')
+else:
+    print('NO')
+
+
